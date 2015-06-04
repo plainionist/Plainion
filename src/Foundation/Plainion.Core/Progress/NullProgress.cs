a52c57f0db49fd2694c2c8e5a574798cfa39a778
ronin4net <stanley751@gmx.net>
2015-06-04 07:15:07 +0200

    Core: NullProgress added

---
+++ src/Foundation/Plainion.Core/Progress/NullProgress.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Plainion.Progress
+{
+    /// <summary>
+    /// Null-object pattern for IProgress{T}
+    /// </summary>
+    public sealed class NullProgress<T> : IProgress<T>
+    {
+        public void Report( T value ) { }
+    }
+
+    /// <summary>
+    /// Null-object pattern for IProgress{IProgressInfo}
+    /// </summary>
+    public sealed class NullProgress : IProgress<IProgressInfo>
+    {
+        public void Report( IProgressInfo value ) { }
+    }
+}
