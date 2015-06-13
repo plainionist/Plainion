892d79b6755ca047a183a0a2ff4ff478a27ce0d7
ronin4net <stanley751@gmx.net>
2015-06-13 22:18:28 +0200

    fix of workspace fix

--- src/AppFw/Plainion.AppFw.Wpf/Services/ProjectService.cs
+++ src/AppFw/Plainion.AppFw.Wpf/Services/ProjectService.cs
@@ -4,6 +4,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Plainion.AppFw.Wpf.Infrastructure;
 using Plainion.Progress;
+using Plainion.Windows;
 
 namespace Plainion.AppFw.Wpf.Services
 {
