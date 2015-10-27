3a00e834fd859cec3871ba86e2329d77e91db8fa
ronin4net <stanley751@gmx.net>
2015-10-27 12:29:49 +0100

    Prism.Tests assembly created

--- src/Foundation/Plainion.Core.Tests/Properties/AssemblyInfo.cs
+++ src/Foundation/Plainion.Core.Tests/Properties/AssemblyInfo.cs
@@ -7,12 +7,8 @@ using System.Security;
 [assembly: AssemblyTitle( "Plainion.Core.Tests" )]
 [assembly: CLSCompliant( true )]
 [assembly: AssemblyVersion( "2.8.0.0" )]
+[assembly: ComVisible(false)]
 
 // required since .NET 4.0 to allow calls to other assemblies
 [assembly: AllowPartiallyTrustedCallersAttribute()]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly : ComVisible( false )]
 
---
+++ src/Foundation/Plainion.Prism.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Plainion.Prism.Tests")]
+[assembly: ComVisible(false)]
+[assembly: Guid("8f83f687-ce24-403a-a20b-04643195da81")]
+[assembly: AssemblyVersion("1.6.0.0")]
