89bb785f63479c51145b6441c48eca26bdcd255d
ronin4net <stanley751@gmx.net>
2015-11-28 07:56:14 +0100

    updated to Prism.Mvvm 1.1.1 Added SelectFolderDialog Added Plainion.RI

---
+++ src/Foundation/Plainion.RI/Shell.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace Plainion.RI
+{
+    public partial class Shell : Window
+    {
+        public Shell()
+        {
+            InitializeComponent();
+
+            DataContext = new ShellViewModel();
+        }
+    }
+}
---
+++ src/Foundation/Plainion.RI/ShellViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.Mvvm;
+using Plainion.Windows.Controls;
+
+namespace Plainion.RI
+{
+    class ShellViewModel : BindableBase
+    {
+        public ShellViewModel()
+        {
+            SelectFolderCommand = new DelegateCommand( OnSelectFolder );
+        }
+
+        public ICommand SelectFolderCommand { get; private set; }
+
+        private void OnSelectFolder()
+        {
+            var dialog = new SelectFolderDialog();
+            dialog.Description = "Select some folder you like";
+            if( dialog.ShowDialog() ==true)
+            {
+                MessageBox.Show( "Selected folder: " + dialog.SelectedPath );
+            }
+        }
+    }
+}
