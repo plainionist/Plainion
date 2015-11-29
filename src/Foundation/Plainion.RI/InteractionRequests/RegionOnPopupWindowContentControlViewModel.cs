65f2502acd9140acbf78837c0f5266c884875cbd
ronin4net <stanley751@gmx.net>
2015-11-29 14:12:12 +0100

    Prism: PopupWindowContentControl added RI: InteractionRequests/RegionOnPopupWindowContentControlView added

---
+++ src/Foundation/Plainion.RI/InteractionRequests/RegionOnPopupWindowContentControlViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.Composition;
+using System.Windows.Input;
+using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.Interactivity.InteractionRequest;
+using Microsoft.Practices.Prism.Mvvm;
+using Plainion.RI.InteractionRequests.Dialogs;
+
+namespace Plainion.RI.InteractionRequests
+{
+    /// <summary>
+    /// This sample defines a region on a PopupWindowContentControl inside PopupWindowAction.WindowContent. This way the requesting
+    /// viewmodel doesnt need to know anything about the concreate view/model.
+    /// <para>
+    /// This approach works well for complex dialogs like "Settings" dialogs.
+    /// This approach supports view importing there viewmodel directly AND supports IInteractionRequestAware for the viewmodel.
+    /// </para>
+    /// </summary>
+    [Export]
+    class RegionOnPopupWindowContentControlViewModel : BindableBase
+    {
+        [ImportingConstructor]
+        public RegionOnPopupWindowContentControlViewModel( Model model )
+        {
+            Model = model;
+
+            ShowConfirmationCommand = new DelegateCommand( OnShowConfirmation );
+            ConfirmationRequest = new InteractionRequest<INotification>();
+        }
+
+        public Model Model { get; private set; }
+
+        public ICommand ShowConfirmationCommand { get; private set; }
+
+        private void OnShowConfirmation()
+        {
+            var notification = new Notification();
+            notification.Title = "Really?";
+
+            // view imported via region, viewmodel imported by view directly
+
+            ConfirmationRequest.Raise( notification, n => { } );
+        }
+
+        public InteractionRequest<INotification> ConfirmationRequest { get; private set; }
+    }
+}
