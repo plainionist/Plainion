d1e3655bfadbdc2ee9e680cf7aff23c3ef865207
ronin4net <stanley751@gmx.net>
2015-11-30 21:31:56 +0100

    RI: documentation improved

--- src/Foundation/Plainion.RI/InteractionRequests/DefaultWindowWithViewModelAsContentViewModel.cs
+++ src/Foundation/Plainion.RI/InteractionRequests/DefaultWindowWithViewModelAsContentViewModel.cs
@@ -1,20 +1,11 @@
 ﻿using System.ComponentModel.Composition;
-using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
 using Microsoft.Practices.Prism.Commands;
 using Microsoft.Practices.Prism.Interactivity.InteractionRequest;
 using Microsoft.Practices.Prism.Mvvm;
 
 namespace Plainion.RI.InteractionRequests
 {
-    /// <summary>
-    /// This sample does not provide any "WindowContent" to the PopupWindowAction. PopupWindowAction will then create a "default" window and 
-    /// assign the notification as DataContext. The default window contains a ContentControl which binds INotification.Content to its Content property.
-    /// <para>
-    /// This approach works well for simple notifications and simple yes/no confirmations to the user.
-    /// </para>
-    /// </summary>
     [Export]
     class DefaultWindowWithViewModelAsContentViewModel : BindableBase
     {
@@ -32,6 +23,7 @@ namespace Plainion.RI.InteractionRequests
         {
             var confirmation = new Confirmation();
             confirmation.Title = "Really?";
+
             // TODO: unfort. i couldnt find a way to provide a DataTemplate for ConfirmationContent to the new window
             confirmation.Content = new ConfirmationContent( "Here goes the question, doesn't it?" );
 
