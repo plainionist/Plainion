5b007b4f6f1697a0a56b9a8a99065b7427d0bdbb
ronin4net <stanley751@gmx.net>
2016-01-14 10:39:44 +0100

    RI: NotePad added

--- src/Foundation/Plainion.RI/CoreModule.cs
+++ src/Foundation/Plainion.RI/CoreModule.cs
@@ -5,10 +5,11 @@ using Microsoft.Practices.Prism.Regions;
 using Plainion.RI.Dialogs;
 using Plainion.RI.InteractionRequests;
 using Plainion.RI.InteractionRequests.Dialogs;
+using Plainion.RI.NotePad;
 
 namespace Plainion.RI
 {
-    [ModuleExport( typeof( CoreModule ) )]
+    [ModuleExport(typeof(CoreModule))]
     class CoreModule : IModule
     {
         [Import]
@@ -16,22 +17,24 @@ namespace Plainion.RI
 
         public void Initialize()
         {
-            RegionManager.RegisterViewWithRegion( RegionNames.Dialogs, typeof( SelectFolderDialogView ) );
-            RegionManager.RegisterViewWithRegion( RegionNames.Dialogs, typeof( SelectFolderInteractionRequestView ) );
+            RegionManager.RegisterViewWithRegion(RegionNames.Dialogs, typeof(SelectFolderDialogView));
+            RegionManager.RegisterViewWithRegion(RegionNames.Dialogs, typeof(SelectFolderInteractionRequestView));
 
-            RegionManager.RegisterViewWithRegion( RegionNames.InteractionRequests, typeof( DefaultWindowWithViewAsContentView ) );
-            RegionManager.RegisterViewWithRegion( RegionNames.InteractionRequests, typeof( DefaultWindowWithViewModelAsContentView ) );
-            RegionManager.RegisterViewWithRegion( RegionNames.InteractionRequests, typeof( CustomNotificationView ) );
-            RegionManager.RegisterViewWithRegion( RegionNames.InteractionRequests, typeof( ComplexCustomViewView ) );
-            RegionManager.RegisterViewWithRegion( RegionNames.InteractionRequests, typeof( RegionOnContentControlView ) );
-            RegionManager.RegisterViewWithRegion( RegionNames.InteractionRequests, typeof( RegionOnPopupWindowActionView ) );
-            RegionManager.RegisterViewWithRegion( RegionNames.InteractionRequests, typeof( RegionOnPopupWindowContentControlView ) );
-            RegionManager.RegisterViewWithRegion( RegionNames.InteractionRequests, typeof( RegionWithPopupWindowActionExtensionsView ) );
+            RegionManager.RegisterViewWithRegion(RegionNames.InteractionRequests, typeof(DefaultWindowWithViewAsContentView));
+            RegionManager.RegisterViewWithRegion(RegionNames.InteractionRequests, typeof(DefaultWindowWithViewModelAsContentView));
+            RegionManager.RegisterViewWithRegion(RegionNames.InteractionRequests, typeof(CustomNotificationView));
+            RegionManager.RegisterViewWithRegion(RegionNames.InteractionRequests, typeof(ComplexCustomViewView));
+            RegionManager.RegisterViewWithRegion(RegionNames.InteractionRequests, typeof(RegionOnContentControlView));
+            RegionManager.RegisterViewWithRegion(RegionNames.InteractionRequests, typeof(RegionOnPopupWindowActionView));
+            RegionManager.RegisterViewWithRegion(RegionNames.InteractionRequests, typeof(RegionOnPopupWindowContentControlView));
+            RegionManager.RegisterViewWithRegion(RegionNames.InteractionRequests, typeof(RegionWithPopupWindowActionExtensionsView));
 
-            RegionManager.RegisterViewWithRegion( "RegionOnContentControlView", typeof( ComplexDialog ) );
-            RegionManager.RegisterViewWithRegion( "RegionOnPopupWindowActionView", typeof( ComplexDialog ) );
-            RegionManager.RegisterViewWithRegion( "RegionOnPopupWindowContentControlView", typeof( ComplexDialog ) );
-            RegionManager.RegisterViewWithRegion( "RegionWithPopupWindowActionExtensionsView", typeof( ComplexDialog ) );
+            RegionManager.RegisterViewWithRegion("RegionOnContentControlView", typeof(ComplexDialog));
+            RegionManager.RegisterViewWithRegion("RegionOnPopupWindowActionView", typeof(ComplexDialog));
+            RegionManager.RegisterViewWithRegion("RegionOnPopupWindowContentControlView", typeof(ComplexDialog));
+            RegionManager.RegisterViewWithRegion("RegionWithPopupWindowActionExtensionsView", typeof(ComplexDialog));
+
+            RegionManager.RegisterViewWithRegion(RegionNames.NotePad, typeof(NotePadView));
         }
     }
 }
