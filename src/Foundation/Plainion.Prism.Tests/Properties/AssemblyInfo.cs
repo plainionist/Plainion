51045d7fb505ae326eb0b8179175fffcf4b60180
ronin4net <stanley751@gmx.net>
2016-03-10 11:08:19 +0100

    Prism: version increased to 2.0.0 AppFw.Wpr: version increased to 3.0.0

--- src/Foundation/Plainion.Prism.Tests/Properties/AssemblyInfo.cs
+++ src/Foundation/Plainion.Prism.Tests/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Plainion.Prism.Tests")]
 [assembly: ComVisible(false)]
 [assembly: Guid("8f83f687-ce24-403a-a20b-04643195da81")]
-[assembly: AssemblyVersion("1.7.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
