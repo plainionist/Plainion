7b88f7fc220cd7d04640dea5825d2a7117c95856
ronin4net <stanley751@gmx.net>
2016-06-08 09:48:46 +0200

    Windows: version increased to 1.21

--- src/Foundation/Plainion.Windows.Tests/Properties/AssemblyInfo.cs
+++ src/Foundation/Plainion.Windows.Tests/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle( "Plainion.Windows.Tests" )]
 [assembly: ComVisible( false )]
-[assembly: AssemblyVersion( "1.20.0.0" )]
+[assembly: AssemblyVersion( "1.21.0.0" )]
 
 [assembly: Guid( "55c49e4a-9afe-44bf-ba4a-975659f92d3d" )]
 
