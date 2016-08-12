998a62b19c20193856455da4791e3adc14f8a1ea
ronin4net <stanley751@gmx.net>
2016-08-12 15:13:16 +0200

    Logging: tests improved

---
+++ src/Foundation/Plainion.Core.Tests/Logging/FileLoggingSinkTests.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Plainion.Logging;
+
+namespace Plainion.Core.Tests.Logging
+{
+    [TestFixture]
+    class FileLoggingSinkTests
+    {
+        [Test]
+        public void Write_WhenCalled_EntryWrittenWithTimestamp()
+        {
+            var logFile = Path.GetTempFileName();
+            var sink = new FileLoggingSink( logFile );
+
+            sink.Open();
+
+            var entry = new LogEntry( LogLevel.Warning, "test" );
+            sink.Write( entry );
+
+            sink.Close();
+
+            var lines = File.ReadAllLines( logFile );
+
+            Assert.That( lines.Single(), Is.EqualTo( string.Format( "{0}|Warning|test", entry.Timestamp ) ) );
+
+            File.Delete( logFile );
+        }
+    }
+}
