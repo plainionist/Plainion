57da41179f4ab2ec30732ae4d6b259fd80fc4b6c
ronin4net <stanley751@gmx.net>
2016-08-13 13:33:10 +0200

    WildcardTests.cs added

---
+++ src/Foundation/Plainion.Core.Tests/Text/WildcardTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using Plainion.Text;
+
+namespace Plainion.Core.Tests.Text
+{
+    [TestFixture]
+    class WildcardTests
+    {
+        [Test]
+        public void IsMatch_SubstringWithoutLeadingWildcard_Fails()
+        {
+            Assert.That( Wildcard.IsMatch( "dummy", "mm*" ), Is.False );
+        }
+
+        [Test]
+        public void IsMatch_SubstringWithoutTrailingWildcard_Fails()
+        {
+            Assert.That( Wildcard.IsMatch( "dummy", "*mm" ), Is.False );
+        }
+
+        [Test]
+        public void IsMatch_SubstringWithLeadingWildcard_Succeeds()
+        {
+            Assert.That( Wildcard.IsMatch( "dummy", "*mmy" ), Is.True );
+        }
+
+        [Test]
+        public void IsMatch_SubstringWithTrailingWildcard_Succeeds()
+        {
+            Assert.That( Wildcard.IsMatch( "dummy", "dum*" ), Is.True );
+        }
+
+        [Test]
+        public void IsMatch_WildcardInMiddle_Succeeds()
+        {
+            Assert.That( Wildcard.IsMatch( "dummy", "d*y" ), Is.True );
+        }
+
+        [Test]
+        public void IsMatch_SingleWildcardReplacesOneCharacter_Succeeds()
+        {
+            Assert.That( Wildcard.IsMatch( "dummy", "d?mmy" ), Is.True );
+        }
+
+    }
+}
