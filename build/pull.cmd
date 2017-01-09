4b6bd8779261e934dba90083953eb29e744f05e4
ronin4net <pinguin751@gmx.net>
2017-01-09 10:29:22 +0100

    pull script added

---
+++ build/pull.cmd
@@ -0,0 +1,31 @@
+@echo off
+setlocal
+
+set git="C:\Program Files\Git\bin\git.exe"
+
+git clone https://github.com/ronin4net/Plainion.git
+git clone https://github.com/ronin4net/Plainion.Windows.git
+git clone https://github.com/ronin4net/Plainion.Windows.Editors.git
+git clone https://github.com/ronin4net/Plainion.Prism.git
+git clone https://github.com/ronin4net/Plainion.AppFw.Wpf.git
+git clone https://github.com/ronin4net/Plainion.AppFw.Shell.git
+
+git clone https://github.com/ronin4net/Plainion.CI.git
+git clone https://github.com/ronin4net/Plainion.IronDoc.git
+git clone https://github.com/ronin4net/Plainion.Flames.git
+git clone https://github.com/ronin4net/Plainion.GraphViz.git
+git clone https://github.com/ronin4net/Plainion.Forest.git
+git clone https://github.com/ronin4net/Plainion.Whiteboard.git
+git clone https://github.com/ronin4net/Plainion.Notes.git
+git clone https://github.com/ronin4net/Plainion.Bees.git
+
+for /f "delims=" %%i in ('dir /ad /b') do (
+  echo --- %%i ---
+  cd %%i
+  %git% pull
+  cd ..
+)
+
+pause
+
+endlocal
