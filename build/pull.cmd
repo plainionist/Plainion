f0d827378b3f008a56be4aa919568d86186b544a
ronin4net <pinguin751@gmx.net>
2017-01-10 21:45:35 +0100

    pull.cmd updated after github user name rename

--- build/pull.cmd
+++ build/pull.cmd
@@ -1,21 +1,21 @@
 @echo off
 setlocal
 
-git clone https://github.com/ronin4net/Plainion.git
-git clone https://github.com/ronin4net/Plainion.Windows.git
-git clone https://github.com/ronin4net/Plainion.Windows.Editors.git
-git clone https://github.com/ronin4net/Plainion.Prism.git
-git clone https://github.com/ronin4net/Plainion.AppFw.Wpf.git
-git clone https://github.com/ronin4net/Plainion.AppFw.Shell.git
+git clone https://github.com/plainionist/Plainion.git
+git clone https://github.com/plainionist/Plainion.Windows.git
+git clone https://github.com/plainionist/Plainion.Windows.Editors.git
+git clone https://github.com/plainionist/Plainion.Prism.git
+git clone https://github.com/plainionist/Plainion.AppFw.Wpf.git
+git clone https://github.com/plainionist/Plainion.AppFw.Shell.git
 
-git clone https://github.com/ronin4net/Plainion.CI.git
-git clone https://github.com/ronin4net/Plainion.IronDoc.git
-git clone https://github.com/ronin4net/Plainion.Flames.git
-git clone https://github.com/ronin4net/Plainion.GraphViz.git
-git clone https://github.com/ronin4net/Plainion.Forest.git
-git clone https://github.com/ronin4net/Plainion.Whiteboard.git
-git clone https://github.com/ronin4net/Plainion.Notes.git
-git clone https://github.com/ronin4net/Plainion.Bees.git
+git clone https://github.com/plainionist/Plainion.CI.git
+git clone https://github.com/plainionist/Plainion.IronDoc.git
+git clone https://github.com/plainionist/Plainion.Flames.git
+git clone https://github.com/plainionist/Plainion.GraphViz.git
+git clone https://github.com/plainionist/Plainion.Forest.git
+git clone https://github.com/plainionist/Plainion.Whiteboard.git
+git clone https://github.com/plainionist/Plainion.Notes.git
+git clone https://github.com/plainionist/Plainion.Bees.git
 
 for /f "delims=" %%i in ('dir /ad /b') do (
   echo --- %%i ---
